d37abc60e73fb3117de241e18bb1af4e8c72ec41
Jose Anania <joseanania47@gmail.com>
2022-03-25 20:19:48 -0300

    first commit

---
+++ PrecioIva/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrecioIva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double precio;
+            double iva;
+            Console.WriteLine("Ingrese el precio neto: ");
+            precio = Convert.ToDouble(Console.ReadLine());
+            iva = (precio * 1.21);
+            Console.WriteLine("Precio con IVA: $" + iva);
+            Console.Read();
+        }
+    }
+}
